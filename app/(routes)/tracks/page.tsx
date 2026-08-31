ca5f1872631039d3a26e134666fa09b6f95d5559
Ryan-Sean Rajpaul <ryanseanrajpaul@gmail.com>
2026-08-31 14:46:07 +0200

    weird track bug is now deployed

--- app/(routes)/tracks/page.tsx
+++ app/(routes)/tracks/page.tsx
@@ -1,9 +1,5 @@
-import Pagination from "@/app/ui/Pagination/pagination";
-
 export default function Home() {
   return (
-    <div className="bg-foreground my-2.5 flex-1 rounded-2xl p-3 text-(--color-text)">
-      <Pagination currentPage={1} totalPages={19} />
-    </div>
+    <div className="bg-foreground my-2.5 flex-1 rounded-2xl p-3 text-(--color-text)"></div>
   );
 }
