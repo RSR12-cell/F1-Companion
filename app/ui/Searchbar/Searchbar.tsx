5639f50844063a6c93bdcd6bb8e6aa1b602b9425
Ryan-Sean Rajpaul <ryanseanrajpaul@gmail.com>
2026-09-01 09:53:50 +0200

    Search Bar component has been created and is able to update the url

--- app/ui/Searchbar/Searchbar.tsx
+++ app/ui/Searchbar/Searchbar.tsx
@@ -1,3 +1,38 @@
+"use client";
+import { Search } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+
 export default function SearchBar({ placeHolder }: { placeHolder: string }) {
-  return <div>{placeHolder}</div>;
+  const pathName = usePathname();
+  const params = useSearchParams();
+  const { replace } = useRouter();
+
+  const handleOnSearch = useDebouncedCallback((term: string) => {
+    const searchParam = new URLSearchParams(params);
+
+    if (term) {
+      searchParam.set("query", term);
+      const path = `${pathName}?${searchParam.toString()}`;
+      replace(path);
+    } else {
+      searchParam.delete("query");
+      replace(`${pathName}?${searchParam.toString()}`);
+    }
+  }, 300);
+
+  return (
+    <div className="bg-foreground border-background text-text relative flex h-10 w-[60%] items-center justify-center rounded-2xl border-2">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <input
+        type="text"
+        className="bg-foreground text-text isolate h-full w-full rounded-2xl px-3 pl-10 text-sm outline-0 peer-focus:text-gray-400 placeholder:text-gray-500 focus:outline-1 focus:outline-slate-600"
+        placeholder={placeHolder}
+        onChange={(e) => handleOnSearch(e.target.value)}
+      />
+      <Search className="peer absolute top-[50%] left-2 z-10 translate-y-[-50%]" />
+    </div>
+  );
 }
